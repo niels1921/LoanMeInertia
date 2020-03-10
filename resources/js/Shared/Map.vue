a614d844e97a9a978fb466bf0393ce02d48a6bb2
Niels <n.nieuwland@outlook.com>
2020-03-10 14:26:53 +0100

    Map

---
+++ resources/js/Shared/Map.vue
@@ -0,0 +1,40 @@
+<template>
+  <MglMap
+      class="map"
+      :accessToken="accessToken"
+      :mapStyle.sync="mapStyle"
+      :zoom="12"
+      :center="[23.130481,63.838491]"
+      @load="onMapLoaded"
+  />
+</template>
+
+<script>
+import Mapbox from 'mapbox-gl'
+import { MglMap } from 'vue-mapbox'
+
+export default {
+  components: {
+    MglMap,
+  },
+  data() {
+    return {
+      accessToken: 'pk.eyJ1IjoibmllbHMxOTIxIiwiYSI6ImNqdG90MHFrbzBwcmU0YW4yMTE1OWZsYTYifQ.DDwwyIVLf_fzq9187gWkAw', // your access token. Needed if you using Mapbox maps
+      mapStyle: 'mapbox://styles/mapbox/streets-v11', // your map style
+    }
+  },
+
+  created() {
+    // We need to set mapbox-gl library here in order to use it in template
+    this.mapbox = Mapbox
+
+  },
+  methods: {
+    // onMapLoaded(event) {
+    //
+    //     this.mapbox.flyTo({ center: [53.20139,5.80859] });
+    //
+    // },
+  },
+}
+</script>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+    <link
+        href="https://api.tiles.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.css"
+        rel="stylesheet"
+    />
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @routes
 </head>
